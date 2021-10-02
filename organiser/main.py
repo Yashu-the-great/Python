f4cb0feda7b7b922258ad5ceda78edd348f987e3
Yashu-the-great <yashuff1@gmail.com>
2021-10-02 23:46:18 +0530

    This is a simple program to organise your directory or files.

---
+++ organiser/main.py
@@ -0,0 +1,21 @@
+import os,shutil
+
+
+def get_extension(file:str) -> str:
+    file = file.split(".")
+    return file[len(file)-1]
+def move_to_file(file:str, ext:str):
+    if os.path.isdir(ext):
+        shutil.move(file,ext)
+    else:
+        os.mkdir(ext)
+        shutil.move(file,ext)
+
+def main():
+    directory = os.getcwd()
+    for file in os.listdir(directory):
+        ext = get_extension(file)
+        move_to_file(file,ext)
+if __name__ == "__main__":
+    main()
+    
