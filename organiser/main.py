1cb5e659188e96dcb8478221874d9d4202feea41
Yashu-the-great <yashuff1@gmail.com>
2021-10-03 00:00:15 +0530

    fixed a bug

--- organiser/main.py
+++ organiser/main.py
@@ -5,11 +5,14 @@ def get_extension(file:str) -> str:
     file = file.split(".")
     return file[len(file)-1]
 def move_to_file(file:str, ext:str):
-    if os.path.isdir(ext):
-        shutil.move(file,ext)
-    else:
-        os.mkdir(ext)
-        shutil.move(file,ext)
+    try:
+        if os.path.isdir(ext):
+           shutil.move(file,ext)
+        else:
+            os.mkdir(ext)
+            shutil.move(file,ext)
+    except:
+        pass
 
 def main():
     directory = os.getcwd()
